fb7676ed98b71a2f15aacbd13ec098555bfac67d
leonardovalenascimento <leonardo.valenascimento@gmail.com>
2018-11-27 17:27:10 -0200

    10% do desenvolvimento

--- CopiaArquivosKTM/Form1.Designer.cs
+++ CopiaArquivosKTM/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnArquivoTreinamento = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblTipoDocumento = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArquivoTreinamento = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArquivoTreinamento
+            // 
+            this.btnArquivoTreinamento.Location = new System.Drawing.Point(373, 121);
+            this.btnArquivoTreinamento.Name = "btnArquivoTreinamento";
+            this.btnArquivoTreinamento.Size = new System.Drawing.Size(107, 20);
+            this.btnArquivoTreinamento.TabIndex = 0;
+            this.btnArquivoTreinamento.Text = "Abrir";
+            this.btnArquivoTreinamento.UseVisualStyleBackColor = true;
+            this.btnArquivoTreinamento.Click += new System.EventHandler(this.btnArquivoTreinamento_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(351, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // lblTipoDocumento
+            // 
+            this.lblTipoDocumento.AutoSize = true;
+            this.lblTipoDocumento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoDocumento.Location = new System.Drawing.Point(12, 34);
+            this.lblTipoDocumento.Name = "lblTipoDocumento";
+            this.lblTipoDocumento.Size = new System.Drawing.Size(183, 20);
+            this.lblTipoDocumento.TabIndex = 2;
+            this.lblTipoDocumento.Text = "Tipos de Documentos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Arquivos para Treinamento";
+            // 
+            // txtArquivoTreinamento
+            // 
+            this.txtArquivoTreinamento.Location = new System.Drawing.Point(16, 121);
+            this.txtArquivoTreinamento.Name = "txtArquivoTreinamento";
+            this.txtArquivoTreinamento.Size = new System.Drawing.Size(351, 20);
+            this.txtArquivoTreinamento.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 178);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(351, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Diretório de Cluster";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 20);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Abrir";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(511, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtArquivoTreinamento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTipoDocumento);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnArquivoTreinamento);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnArquivoTreinamento;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblTipoDocumento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtArquivoTreinamento;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CopiaArquivosKTM/Form1.cs
+++ CopiaArquivosKTM/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,46 @@ namespace CopiaArquivosKTM
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnArquivoTreinamento_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog opdArquivosTreinamento = new OpenFileDialog();
+
+                opdArquivosTreinamento.InitialDirectory = @"D:\";
+                opdArquivosTreinamento.RestoreDirectory = true;
+                opdArquivosTreinamento.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                opdArquivosTreinamento.CheckFileExists = true;
+                opdArquivosTreinamento.CheckPathExists = true;
+                
+
+                DialogResult resultado = opdArquivosTreinamento.ShowDialog();
+
+                if (resultado == DialogResult.OK)
+                {
+                    txtArquivoTreinamento.Text = opdArquivosTreinamento.FileName;
+                }
+                else
+                {
+                    throw new Exception("Erro ao ler o arquivo");
+                }
+            }
+            catch (SecurityException ex)
+            {
+                // O usuário  não possui permissão para ler arquivos
+                MessageBox.Show("Erro de segurança Contate o administrador de segurança da rede.\n\n" +
+                                            "Mensagem : " + ex.Message + "\n\n" +
+                                            "Detalhes (enviar ao suporte):\n\n" + ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                // Não pode carregar a imagem (problemas de permissão)
+                MessageBox.Show("Não é possível exibir a imagem : " /*+ arquivo.Substring(file.LastIndexOf('\\'))*/
+                                           + ". Você pode não ter permissão para ler o arquivo , ou " +
+                                           " ele pode estar corrompido.\n\nErro reportado : " + ex.Message);
+            }
         }
     }
 }
